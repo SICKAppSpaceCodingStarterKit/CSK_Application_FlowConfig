ce550e8af53c2ceaccbf08e8a40db34906cdad47
Roy Gollub <roy.gollub@sick.de>
2025-12-15 11:11:00 +0100

    Squashed 'subtrees/CSK_Module_FlowConfig/' content from commit ef3ad04
    git-subtree-dir: subtrees/CSK_Module_FlowConfig
    git-subtree-split: ef3ad0404c3d049380ce1395e7475e6e1b936b77

---
+++ CSK_Module_FlowConfig/scripts/CSK_FlowConfig_FlowConfig.lua
@@ -0,0 +1,12 @@
+--*****************************************************************
+-- Here you will find all the required content to provide specific
+-- features of this module via the 'CSK FlowConfig'.
+-- This specific script will run in its own thread (special use case, see FlowConfig_Model.lua)
+--*****************************************************************
+
+_G.availableAPIs = require('Communication/FlowConfig/helper/checkAPIs')
+
+require('Communication.FlowConfig.Features.FlowConfig_OnEvent')
+require('Communication.FlowConfig.Features.FlowConfig_NotifyEvent')
+require('Communication.FlowConfig.Features.FlowConfig_OnExpired')
+require('Communication.FlowConfig.Features.FlowConfig_ProcessLogic')
